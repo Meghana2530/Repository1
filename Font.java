e0509eaa237f70e89b8aff7f8099ad808263c378
Ashu Meghana Nagaraj <AshuMeghana.Nagaraj@jda.com>
2019-04-27 13:54:59 +0530

    Added file to branch 2

---
+++ Font.java
@@ -0,0 +1,26 @@
+package seleniumFull;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Font {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System. setProperty("webdriver.chrome.driver", "C:/Users/1021207/chromedriver_win32/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		//To open file saved on local 
+		driver.get("https://www.google.com");
+		WebElement gmail= driver.findElement(By.linkText("Gmail"));
+		String fontSize=gmail.getCssValue("font-size");
+		String fontColor=gmail.getCssValue("color");
+		System.out.println(fontSize);
+		System.out.println(fontColor);
+		driver.close();
+		//style on browser 
+	}
+
+}
